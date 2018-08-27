fad0af271d58bb9e7069e1239f414c438eb83a5c
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-27 21:06:25 +0530

    degrres_to_farenheit.java

---
+++ cspp2-practice/m1/degrres_to_farenheit.java
@@ -0,0 +1,9 @@
+class Degrees_toFahrenheit{
+	public static void main(String args[]){
+		double fahrenheit;
+		double degreesCelsius = 10.0;
+		fahrenheit = degreesCelsius*(9.0/5.0)+32;
+		System.out.println(fahrenheit);
+	}
+
+}
