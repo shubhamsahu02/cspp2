8a8ad1008ac50c7f753b63be0c8ba3b1c1162853
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-08-27 20:48:01 +0530

    swap.java

---
+++ cspp2-practice/m1/swap.java
@@ -0,0 +1,10 @@
+public class swap {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int c = a;
+        a = b;
+        b = c;
+System.out.println("The swapped numbers are : " +a +","+b);
+    }
+}
